b014e90b94873909ec890487692e3b15e7c45e00
Alin Florin <alin@qoffice.ro>
2023-12-07 13:38:17 +0200

    d

--- src/routes/ota.route.ts
+++ src/routes/ota.route.ts
@@ -3,11 +3,6 @@ import {readFile} from 'fs/promises';
 
 const otaRouter = Router();
 
-otaRouter.get("/api/ota/version", async (req: Request, res: Response) => {
-  const file = (await readFile('/firmware/VERSION.txt')).toString('utf8');
-  res.send(file);
-});
-
 otaRouter.get("/api/ota/update", async (req: Request, res: Response) => {
   const file = await readFile('/firmware/firmware.bin');
   res.send(file);
